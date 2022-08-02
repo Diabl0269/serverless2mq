d349aef8c0e45df75784df4e66f7bb5728b86c4d
Omer Malka <omermalka@OmerMa-MacBook-Pro.local>
2022-08-02 15:27:30 +0300

    new consume.ts file

--- consume.ts
+++ consume.ts
@@ -1,6 +1,7 @@
 import { argv } from "process";
 import { defaultUrl, exchange, routingKeyPrefix, keys } from "./src/config";
 import amqp from "amqplib";
+import { Consumer } from "./src/consumer";
 
 export const getKey = () => {
   const key = argv[argv.length - 2] as typeof keys[number];
@@ -20,14 +21,9 @@ const getNodeNum = () => {
 export const consume = async () => {
   const key = getKey();
   const nodeNum = getNodeNum();
-  const connection = await amqp.connect(defaultUrl);
-  const channel = await connection.createChannel();
-  await channel.assertExchange(exchange.name, exchange.type);
-  const q = await channel.assertQueue("", {
-    autoDelete: true,
-  });
-  await channel.bindQueue(q.queue, exchange.name, `${routingKeyPrefix}${key}`);
-  await channel.consume(q.queue, (msg) => {
+  const consumer = new Consumer();
+  await consumer.connect(key);
+  await consumer.consume((msg) => {
     console.log(`${key}-${nodeNum}: Consumed message successfully!
         Content: ${msg?.content.toString()}`);
   });
